0ec7b4882d730c5f7eb2030e1a45cb6b4d1887b9
Latada <icom92@naver.com>
2023-03-02 00:23:01 +0900

    [Feat] How to Responses

--- src/server.js
+++ src/server.js
@@ -3,8 +3,17 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
-const handleHome = () => console.log("Somebody is trying to go home");
+const handleHome = (req, res) => {
+  console.log("Somebody is trying to go home");
+  return res.send("<h1>전송완료</h1>");
+};
+const handleLogin = (req, res) => {
+  console.log("로그인을 시도합니다.");
+  return res.send("로그인하세요.");
+};
+
 app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 const handleListening = () =>
   console.log(`Server listening on http://localhost:${PORT} 🚀`);
