34d4c78ad834ecb2637a57384a007b4e70a67a7a
Latada <icom92@naver.com>
2023-03-02 00:09:48 +0900

    [Feat] GET Request

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
+const handleHome = () => console.log("Somebody is trying to go home");
+app.get("/", handleHome);
+
 const handleListening = () =>
   console.log(`Server listening on http://localhost:${PORT} 🚀`);
 
